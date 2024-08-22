0f8eaa1d7816e2cbc2568d6600c076121b585eae
Hérson Rezende <herson.academico@gmail.com>
2024-08-22 11:41:25 -0300

    feat(): ordenacao da lista de metas

--- lib/src/providers/goal_provider.dart
+++ lib/src/providers/goal_provider.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
+import '../enums/goal_type_enum.dart';
 import '../models/goal.dart';
 import '../models/food.dart';
 import 'food_provider.dart';
@@ -12,6 +13,10 @@ class GoalNotifier extends StateNotifier<List<Goal>> {
 
   GoalNotifier(this.ref) : super([]) {
     _loadGoalsAndUpdateStatus();
+
+    ref.listen<List<Food>>(foodProvider, (previous, next) {
+      _updateGoalsStatus();
+    });
   }
 
   Future<void> _loadGoalsAndUpdateStatus() async {
@@ -24,10 +29,30 @@ class GoalNotifier extends StateNotifier<List<Goal>> {
     final goalsString = prefs.getString('goals');
 
     if (goalsString != null) {
-      final loadedGoals = (json.decode(goalsString) as List).map((goalJson) => Goal.fromMap(goalJson)).toList()
-        ..sort((a, b) => a.startsAt.compareTo(b.startsAt));
+      final loadedGoals = (json.decode(goalsString) as List).map((goalJson) => Goal.fromMap(goalJson)).toList();
+
+      // Ordena as metas conforme especificado
+      loadedGoals.sort((a, b) {
+        // Primeiro, prioriza metas não atingidas sobre as atingidas
+        if (a.achieved != b.achieved) {
+          return a.achieved! ? 1 : -1;
+        }
+        // Dentro dos grupos, ordena pelo tipo de meta
+        return _compareGoalTypes(a.type, b.type);
+      });
+
       state = loadedGoals;
     }
+  }
+
+  int _compareGoalTypes(GoalTypeEnum a, GoalTypeEnum b) {
+    const typeOrder = {
+      GoalTypeEnum.daily: 0,
+      GoalTypeEnum.weekly: 1,
+      GoalTypeEnum.monthly: 2,
+      GoalTypeEnum.yearly: 3,
+    };
+    return typeOrder[a]!.compareTo(typeOrder[b]!);
   }
 
   void _updateGoalsStatus() {
@@ -35,11 +60,7 @@ class GoalNotifier extends StateNotifier<List<Goal>> {
 
     state = state.map((goal) {
       final totalCalories = _calculateCaloriesForGoal(goal, foods);
-      if (totalCalories >= goal.target) {
-        return goal.copyWith(achieved: true);
-      } else {
-        return goal.copyWith(achieved: false);
-      }
+      return goal.copyWith(achieved: totalCalories >= goal.target);
     }).toList();
 
     _saveGoals();
@@ -53,21 +74,34 @@ class GoalNotifier extends StateNotifier<List<Goal>> {
   }
 
   Future<void> addGoal(Goal goal) async {
-    state = [...state, goal]..sort((a, b) => a.startsAt.compareTo(b.startsAt));
+    state = [...state, goal];
     _updateGoalsStatus();
+    _sortGoals(); // Ordena novamente após atualizar o status das metas
     await _saveGoals();
   }
 
   Future<void> editGoal(Goal editedGoal) async {
-    state = state.map((goal) => goal.id == editedGoal.id ? editedGoal : goal).toList()
-      ..sort((a, b) => a.startsAt.compareTo(b.startsAt));
+    state = state.map((goal) => goal.id == editedGoal.id ? editedGoal : goal).toList();
     _updateGoalsStatus();
+    _sortGoals(); // Ordena novamente após atualizar o status das metas
     await _saveGoals();
   }
 
   Future<void> removeGoal(int id) async {
     state = state.where((goal) => goal.id != id).toList();
+    _sortGoals(); // Ordena novamente após remover a meta
     await _saveGoals();
+  }
+
+  void _sortGoals() {
+    state.sort((a, b) {
+      // Primeiro, prioriza metas não atingidas sobre as atingidas
+      if (a.achieved != b.achieved) {
+        return a.achieved! ? 1 : -1;
+      }
+      // Dentro dos grupos, ordena pelo tipo de meta
+      return _compareGoalTypes(a.type, b.type);
+    });
   }
 
   Future<void> _saveGoals() async {
